f60b72a36facadef91e49f8438cca643e53e6288
William Clifford <williamdavidclifford@gmail.com>
2019-11-14 12:25:46 +1100

    added comments

--- Main.java
+++ Main.java
@@ -7,11 +7,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Calls the readNumber method to get the user input for the principal, annual interest and period
+
         int principal = (int)readNumber("Princple: ", 1000, 1000000);
         float annualInterest = (float)readNumber("Annual Interest Rate: ", 1, 30);
         byte years = (byte)readNumber("Period (Years): ", 1, 30);
 
+        // Calls the calculateMortgage method to work out the monthly repayments
+
         double mortgage = calculateMortgage(principal, annualInterest, years);
+
+        // Formats the monthly repayments to currency
 
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Mortgage: " + mortgageFormatted);
@@ -19,6 +25,9 @@ public class Main {
 
 
     public static double readNumber(String prompt, double min, double max){
+
+        // Prompts the user to input a value for the principal, annual interest and period
+        // Will re-prompt the user if the value is not within the min and max
 
         Scanner scanner = new Scanner(System.in);
         double value;
@@ -35,6 +44,9 @@ public class Main {
 
 
     public static double calculateMortgage(int principal, float annualInterest, byte years) {
+
+        // Calculates monthly repayment amounts by:
+        // M = P( r(1+r)^n  /   (1 + r)^n - 1 )
 
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
